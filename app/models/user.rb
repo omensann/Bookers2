e2444b8bf0fd867914ddcf972a6e6067f61765ab
omensann <135625902+omensann@users.noreply.github.com>
2023-07-28 13:31:22 +0000

    [add]Bookers2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :image
+  has_many :books, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :books
+  resources :users, only:[:show, :index, :edit]
+  get '/homes/about' => "homes#about", as: "about"
   devise_for :users
+  root to: "homes#top", as: "top"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
