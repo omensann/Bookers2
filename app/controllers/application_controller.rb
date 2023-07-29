4cbad8b23c2894a7ad01a762590b251e918875b7
omensann <135625902+omensann@users.noreply.github.com>
2023-07-29 10:34:20 +0000

    [add]Bookers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resouce)
-    top_path  #トップには移行しない[/users/id]に移動できるようにしよう。
+    users_path  #トップには移行しない[/users/id]に移動できるようにしよう。
   end
 
   def after_sign_out_path_for(resouce)
-    top_path  #トップには移行しない[/users/id]に移動できるようにしよう。
+    top_path
   end
 
   protected
