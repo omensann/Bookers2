3cd398aa6e3c3cc1c357d5d580cb870dd72d042b
omensann <135625902+omensann@users.noreply.github.com>
2023-08-24 09:30:57 +0000

    [add]Booker2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
       flash[:notice] = "投稿に成功しました。"
       redirect_to  book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -32,8 +33,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path
+    if @book.update(book_params)
+      flash[:notice] = "更新に成功しました。"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = "編集に成功しました。"
+      flash[:notice] = "更新に成功しました。"
       redirect_to  user_path(@user.id)
     else
       render :edit
